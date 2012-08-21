3740a60e3a55099601d7ffd3ac8cff00f094a3c6
Aleksei Ptashkin <alexpta@gmail.com>
2012-08-21 20:49:12 +0300

    Add debug logs

--- src/com/alexpta/jdcalc/JDCalculator.java
+++ src/com/alexpta/jdcalc/JDCalculator.java
@@ -14,7 +14,7 @@ public class JDCalculator {
     	long a = (14 - month)/12;
     	long y = year + 4800 - a;
     	long m = month + 12 * a - 3;
-    	//System.out.println("a=" + a + ", y=" + y + ", m=" + m);
+        System.out.println("JDCalculator.getJDN: a=" + a + ", y=" + y + ", m=" + m);
     	long jdn = day + (153 * m + 2) / 5 + 365 * y + y / 4 - (jc ? 0 : y / 100) + (jc ? 0 : y / 400) - (jc ? 32083 : 32045);
     	return jdn;
     }
@@ -42,7 +42,7 @@ public class JDCalculator {
 			long d = (4 * c  + 3) / 1461;
 			long e = c - 1461 * d / 4;
 			long m = (5 * e + 2) / 153;
-			System.out.println("c=" + c + ", d=" + d + ", e=" + e + ", m=" + m);
+			System.out.println("JDCalculator.getDate: c=" + c + ", d=" + d + ", e=" + e + ", m=" + m);
 			day = (int)e - (int)((153 * m + 2) / 5) + 1;
 			month = (int)m + 3 - 12 * ((int)m / 10);
 			year = (int)d - 4800 + (int)m / 10;
@@ -54,7 +54,7 @@ public class JDCalculator {
 			long d = (4 * c  + 3) / 1461;
 			long e = c - 1461 * d / 4;
 			long m = (5 * e + 2) / 153;
-			System.out.println("a=" + a + ", b=" + b + ", c=" + c + ", d=" + d + ", e=" + e + ", m=" + m);
+			System.out.println("JDCalculator.getDate: a=" + a + ", b=" + b + ", c=" + c + ", d=" + d + ", e=" + e + ", m=" + m);
 			day = (int)e - (int)((153 * m + 2) / 5) + 1;
 			month = (int)m + 3 - 12 * ((int)m / 10);
 			year = (int)(100 * b + d - 4800 + m / 10);
